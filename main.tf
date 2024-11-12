5c5dfb0751c688409f8a0748f1c379c2c6eda1c1
Nishanth Prem <nishanthprem8@gmail.com>
2024-11-11 18:51:46 -0700

    Updated terraform file.

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_dynamodb_table" "cloud-api-db" {
   name           = var.db_name
   billing_mode   = "PROVISIONED"
@@ -28,24 +30,46 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
-resource "aws_iam_policy_attachment" "lambda_policy_attach" {
-  name       = "lambda_policy_attachment"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_policy" "lambda_dynamodb_policy" {
+  name        = "lambda_dynamodb_access_policy"
+  description = "Allows Lambda function to access DynamoDB"
+  
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "dynamodb:GetItem",
+          "dynamodb:Query"
+        ],
+        "Resource": "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/cloud-api-db"
+      }
+    ]
+  })
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+}
 
-resource "aws_lambda_function" "my_lambda" {
-  function_name = "my_lambda_function"
+resource "aws_lambda_function" "cloud_api_lambda" {
+  function_name = "cloudApiLambda"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
 
-  filename      = "lambda_function.zip"
+  filename = "lambda_function.zip"
 }
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_file = "lambda_function.py"
   output_path = "lambda_function.zip"
+}
+
+resource "aws_lambda_function_url" "lambda_url" {
+  function_name = aws_lambda_function.cloud_api_lambda.function_name
+  authorization_type = "NONE"
 }
